4520459ab2b99722094e40ae9d7d7704e9fe7287
felixacdc <pfmata01@gmail.com>
2018-06-06 10:04:45 -0600

    [add] Curso de ASP NET MVC en C# Creando el primer controlador y vista #1

---
+++ CursoMVC/CursoMVC/Controllers/PersonaController.cs
@@ -0,0 +1,23 @@
+﻿using CursoMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CursoMVC.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Persona()
+        {
+            Persona obj = new Persona();
+            obj.edad = 24;
+            obj.nombre = "Felix";
+            obj.apellido = "Mendez";
+
+            return View(obj);
+        }
+    }
+}
---
+++ CursoMVC/CursoMVC/Models/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CursoMVC.Models
+{
+    public class Persona
+    {
+        public int edad { get; set; }
+        public String nombre { get; set; }
+        public String apellido { get; set; }
+    }
+}
