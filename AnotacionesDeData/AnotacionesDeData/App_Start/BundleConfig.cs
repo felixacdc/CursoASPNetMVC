2e31fc5c2370367e8b235d68f823527c9f13523f
felixacdc <pfmata01@gmail.com>
2018-06-19 10:26:39 -0600

    [add] Curso de ASP NET MVC en C# Validaciones y empaquetado #8 Part 2

---
+++ AnotacionesDeData/AnotacionesDeData/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace AnotacionesDeData.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bunbles)
+        {
+            bunbles.Add(new ScriptBundle("~/bunbles/jquery").Include("~/Scripts/jquery-{version}.js"));
+            bunbles.Add(new ScriptBundle("~/bunbles/jqueryval").Include("~/Scripts/jquery.validate*"));
+            bunbles.Add(new ScriptBundle("~/bunbles/jqueryvalunobtrusive").Include("~/Scripts/jquery.validate.unobtrusive.min.js"));
+        }
+    }
+}
