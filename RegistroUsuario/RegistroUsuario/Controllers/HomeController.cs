8bec31f28e78549d7b117f0388a2e199d93bee2b
felixacdc <pfmata01@gmail.com>
2018-06-20 11:52:23 -0600

    [add]

--- RegistroUsuario/RegistroUsuario/Controllers/HomeController.cs
+++ RegistroUsuario/RegistroUsuario/Controllers/HomeController.cs
@@ -13,5 +13,10 @@ namespace RegistroUsuario.Controllers
         {
             return View();
         }
+
+        public ActionResult Usuarios()
+        {
+            return View();
+        }
     }
 }
