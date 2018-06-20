22d2ce6bc6c5041f42758d911cc3c7236242876c
felixacdc <pfmata01@gmail.com>
2018-06-20 11:31:09 -0600

    [add] Curso de ASP NET MVC en C# Creando la clase LINQ #9 parte 2

---
+++ RegistroUsuario/RegistroUsuario/Models/UserLogin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RegistroUsuario.Models
+{
+    public class UserLogin
+    {
+        [EmailAddress]
+        [Required(ErrorMessage = "<font color='red'>El email es requerido</font>")]
+        [Display(Name = "Correo Electronico")]
+        public String Email { get; set; }
+
+        [StringLength(100, ErrorMessage = "<font color='red'>El numero de caracteres de {0} debe ser al menos {2}</font>", MinimumLength = 6)]
+        [Required(ErrorMessage = "<font color='red'>El password es requerido</font>")]
+        [Display(Name = "Contraseña")]
+        public String Password { get; set; }
+
+        public String UserName { get; set; }
+    }
+}
